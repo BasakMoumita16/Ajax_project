2f68bb4430efc80df5c3a7cd1233f0922caf3f56
BasakMoumita16 <moumitaduttabasak@gmail.com>
2019-11-20 22:37:49 +0100

    added changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+const app = () => {
+    const APP_ID= "3a1a3ab4";
+    const APP_KEY = "d1559d3e87e73a1d721adcee728b18c8";
+
+const exampleReq =  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+return (
+    
+)
