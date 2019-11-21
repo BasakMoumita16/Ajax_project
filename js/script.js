2e0409418fb751c261e1e5198bb86ad6688fb4fb
BasakMoumita16 <moumitaduttabasak@gmail.com>
2019-11-21 15:39:07 +0100

    fetch Api done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+// below is confidential Api key //
 
-const app = () => {
-    const APP_ID= "3a1a3ab4";
-    const APP_KEY = "d1559d3e87e73a1d721adcee728b18c8";
+const APP_KEY = "d1559d3e87e73a1d721adcee728b18c8";
 
-const exampleReq =  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-return (
-    
-)
+// Async ApI //
+
+async function recipeName() {
+const APIcall =  `https://api.edamam.com/search?q=chicken&app_key=`+ APP_KEY;
+const input = document.getElementById("name").value
+console.log( input);
+
+// fetch method //
+
+await fetch(`https://api.edamam.com/search?q=${input}&app_id=3a1a3ab4&app_key=d1559d3e87e73a1d721adcee728b18c8`)
+.then(res => res.json())
+.then(result => {
+    console.log(result);
+}).catch(err => {
+console.error(err)
+})
+};
+document.getElementById('run').addEventListener('click', recipeName );
