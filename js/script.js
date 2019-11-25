2346c761615ee6946fa1dd44f69a65ddb6f3f66c
BasakMoumita16 <moumitaduttabasak@gmail.com>
2019-11-25 13:18:16 +0100

    Add

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // below is confidential Api key //
 
 const APP_KEY = "d1559d3e87e73a1d721adcee728b18c8";
+const div = document.querySelector(".div");
+console.log(div);
+
 
 // Async ApI here added a extra line of code for resolve the access deny problem//
 
 async function recipeName() 
-
 {
 const APIcall =  (`https://api.edamam.com/search?q=chicken&app_key=`+ APP_KEY , {
 mode: 'no-cors',
@@ -24,41 +26,44 @@ await fetch(`https://api.edamam.com/search?q=${input}&app_id=3a1a3ab4&app_key=d1
 .then(result =>
 {
 console.log(result);
+result.hits.forEach(element => {
+div.appendChild(show_name(element.recipe.label,element.recipe.shareAs));
+div.appendChild(show_image(element.recipe.image, 150,250,'k'))
+element.recipe.ingredientLines.forEach(element => {
+    div.appendChild(show_ingredient(element))
+})
 
-for (let i = 0; i< 10 ; i++)
-{
-show_calorie(result.hits[i].recipe.ingredientLines);
-console.log(result.hits[i].recipe.ingredientLines);
-
-}
-
-for (let i =0; i< 10 ; i++)
-{
-show_image(result.hits[i].recipe.image);
-console.log(result.hits[i].recipe.image);
-}
+});
 
 }).catch(err => {
 console.error(err)
 })
 
-};
-function show_calorie(src) 
-{
-    var line = document.createElement("lines");
-    line.src = src;
-    document.body.appendChild(line);
-};
+// Functions for ingredients//
 
+function show_ingredient(src) 
+{
+var ing = document.createElement('p');
+ing.innerHTML = src;
+return ing;
+};
+// Functions for recipe link//
+function show_name(name,src) 
+{
+var nam = document.createElement('a');
+nam.innerHTML = name;
+nam.setAttribute('href', src)
+return nam;
+};
+// Functions for images//
 function show_image(src, width, height, alt) 
 {
-    var img = document.createElement("img");
-    img.src = src;
-    img.width = width;
-    img.height = height;
-    img.alt = alt;
-    document.body.appendChild(img);
-    
+var img = document.createElement("img");
+img.src = src;
+img.width = width;
+img.height = height;
+img.alt = alt;
+return img;
 };
-
+};
 document.getElementById('run').addEventListener('click', recipeName );
